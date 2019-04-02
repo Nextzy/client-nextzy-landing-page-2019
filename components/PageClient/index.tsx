51461be84978b61faf8b8af03b6a467fa7176b47
Mullika <anterorant@gmail.com>
2019-04-02 16:12:43 +0700

    [🐛] Fix transform number

--- components/PageClient/index.tsx
+++ components/PageClient/index.tsx
@@ -46,7 +46,7 @@ const DataTest = [
     rotate: 49,
     positionSVG: { cx: '647.47', cy: '162.74' },
     fixselected: 'second',
-    transformText: 'translate(684.55 160.29) rotate(-50)',
+    transformText: 'translate(684.55 160.29) rotate(-48)',
     menu: { title: 'MY AIS Application', descrition: 'An' }
   },
   {
@@ -54,7 +54,7 @@ const DataTest = [
     rotate: 28,
     positionSVG: { cx: '721.4', cy: '265.02' },
     fixselected: 'third',
-    transformText: 'translate(750 267.82) rotate(-30)',
+    transformText: 'translate(755 275) rotate(-28)',
     menu: { title: 'MY AIS Application', descrition: 'An' }
   },
   {
@@ -70,7 +70,7 @@ const DataTest = [
     rotate: -20,
     positionSVG: { cx: '716.17', cy: '535.86' },
     fixselected: 'fifth',
-    transformText: 'translate(735 560) rotate(10)',
+    transformText: 'translate(735 565) rotate(20)',
     menu: { title: 'MY AIS Application', descrition: 'An' }
   },
   {
@@ -78,7 +78,7 @@ const DataTest = [
     rotate: -45,
     positionSVG: { cx: '647.47', cy: '629.24' },
     fixselected: 'sixth',
-    transformText: 'translate(657 660) rotate(40)',
+    transformText: 'translate(657 665) rotate(40)',
     menu: { title: 'MY AIS Application', descrition: 'An' }
   },
   {
@@ -86,7 +86,7 @@ const DataTest = [
     rotate: -60,
     positionSVG: { cx: '565.99', cy: '685.43' },
     fixselected: 'seventh',
-    transformText: 'translate(555 720) rotate(50)',
+    transformText: 'translate(555 720) rotate(60)',
     menu: { title: 'MY AIS Application', descrition: 'An' }
   }
 ]
