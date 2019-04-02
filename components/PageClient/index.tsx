92efb168f63361c1a29b165b538c0ef2c1a76d11
Mullika <anterorant@gmail.com>
2019-04-02 17:10:42 +0700

    [🚧] Update product detail

--- components/PageClient/index.tsx
+++ components/PageClient/index.tsx
@@ -39,7 +39,12 @@ const DataTest = [
     positionSVG: { cx: '561.37', cy: '104.12' },
     fixselected: 'first',
     transformText: 'translate(590.63 92) rotate(-61)',
-    menu: { title: 'MY AIS Application', descrition: 'An' }
+    menu: {
+      title: 'MY AIS Application',
+      descrition:
+        'An application that answers all your needs with convenient access to all AIS services 24 hours a day',
+      devices: ['WEB', 'IOS', 'ANDROID']
+    }
   },
   {
     id: 2,
@@ -47,7 +52,7 @@ const DataTest = [
     positionSVG: { cx: '647.47', cy: '162.74' },
     fixselected: 'second',
     transformText: 'translate(684.55 160.29) rotate(-48)',
-    menu: { title: 'MY AIS Application', descrition: 'An' }
+    menu: { title: 'MY AIS Application', descrition: 'An', devices: ['WEB', 'IOS', 'ANDROID'] }
   },
   {
     id: 3,
@@ -55,7 +60,7 @@ const DataTest = [
     positionSVG: { cx: '721.4', cy: '265.02' },
     fixselected: 'third',
     transformText: 'translate(755 275) rotate(-28)',
-    menu: { title: 'MY AIS Application', descrition: 'An' }
+    menu: { title: 'MY AIS Application', descrition: 'An', devices: ['WEB', 'IOS', 'ANDROID'] }
   },
   {
     id: 4,
@@ -63,7 +68,7 @@ const DataTest = [
     positionSVG: { cx: '749.23', cy: '406.21' },
     fixselected: 'fourth',
     transformText: 'translate(768 425) rotate(0)',
-    menu: { title: 'MY AIS Application', descrition: 'An' }
+    menu: { title: 'MY AIS Application', descrition: 'An', devices: ['WEB', 'IOS', 'ANDROID'] }
   },
   {
     id: 5,
@@ -71,7 +76,7 @@ const DataTest = [
     positionSVG: { cx: '716.17', cy: '535.86' },
     fixselected: 'fifth',
     transformText: 'translate(735 565) rotate(20)',
-    menu: { title: 'MY AIS Application', descrition: 'An' }
+    menu: { title: 'MY AIS Application', descrition: 'An', devices: ['WEB', 'IOS', 'ANDROID'] }
   },
   {
     id: 6,
@@ -79,7 +84,7 @@ const DataTest = [
     positionSVG: { cx: '647.47', cy: '629.24' },
     fixselected: 'sixth',
     transformText: 'translate(657 665) rotate(40)',
-    menu: { title: 'MY AIS Application', descrition: 'An' }
+    menu: { title: 'MY AIS Application', descrition: 'An', devices: ['WEB', 'IOS', 'ANDROID'] }
   },
   {
     id: 7,
@@ -87,7 +92,7 @@ const DataTest = [
     positionSVG: { cx: '565.99', cy: '685.43' },
     fixselected: 'seventh',
     transformText: 'translate(555 720) rotate(60)',
-    menu: { title: 'MY AIS Application', descrition: 'An' }
+    menu: { title: 'MY AIS Application', descrition: 'An', devices: ['WEB', 'IOS', 'ANDROID'] }
   }
 ]
 const Home = (): React.FC => {
@@ -103,7 +108,7 @@ const Home = (): React.FC => {
           <TextNEXTZY>
             <Fade right cascade>
               <SectionHeader title="Page Products" />
-              <Product activeProduct={activeProduct} />
+              <Product activeProduct={activeProduct} productData={DataTest} />
             </Fade>
           </TextNEXTZY>
         </ContainerPageObjective>
