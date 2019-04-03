b9270f7a582ab6bf46b586ccd7c9fc9b5711b552
tanapruk <trust@nextzy.com>
2019-04-03 16:05:28 +0700

    Refactor ScrollPage name

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Head from '../components/head'
 import GlobalStyle from '../components/common/GlobalStyle'
-import ScrollPage from '../components/ScrollPage'
+import ScrollPage from '../components/XcrollPage'
 
 const Container = styled.div`
   width: 100%;
