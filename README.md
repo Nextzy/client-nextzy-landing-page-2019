251500f431b156cf3163af709ccdefe8ed293943
Krittamate Khamkom <krittm.kk@gmail.com>
2021-07-12 13:45:56 +0700

    Remove all assets, remaining only index.html

--- README.md
+++
@@ -1,2 +0,0 @@
-# client-nextzy-landing-page-2019
-The awesome landing page for who want to view our pirate
--- utils/decorate-url.tsx
+++
@@ -1,9 +0,0 @@
-import { links } from '../components/common/nav'
-import Router from 'next/router'
-export const addPath = (index): void => {
-  const currentLink = links.filter((link) => link.key === index)[0]
-  const original = `/?goto=${currentLink.href}`
-  const as = `/${currentLink.href}`
-  Router.replace(original, original, { shallow: true })
-  // Router.replace(original, as, { shallow: true })
-}
